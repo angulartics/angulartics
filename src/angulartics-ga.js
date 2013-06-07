6e2eb3fd55f5e0d4fb6f68831c680de3c3b2a3bd
Will McClellan <will@happycry.co.uk>
2013-06-07 10:56:36 +0100

    Optional universal analytics config

--- src/angulartics-ga.js
+++ src/angulartics-ga.js
@@ -6,10 +6,12 @@ angular.module('angulartics.ga', ['angulartics'])
 
     $analyticsProvider.registerPageTrack(function(path) {
       window._gaq.push(['_trackPageview', path]);
+      // ga('send', 'pageview', path);
     });
 
     $analyticsProvider.registerEventTrack(function(category, action, label, value, implicitCount) {
       window._gaq.push(['_trackEvent', category, action, label, value, implicitCount]);
+      // ga('send', 'event', category, action, label, value);
     });
 
   });
