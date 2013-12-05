703a8bc5290815fc432a46afb305088453899c9d
Malte Legenhausen <mlegenhausen@gmail.com>
2013-12-05 11:28:33 +0100

    Cordova/Phonegap Support added via the GAPlugin

---
+++ src/angulartics-google-analytics-cordova.js
@@ -0,0 +1,88 @@
+/**
+ * @license Angulartics v0.8.5
+ * (c) 2013 Luis Farzati http://luisfarzati.github.io/angulartics
+ * License: MIT
+ */
+(function(angular) {
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angulartics.google.analytics
+ * Enables analytics support for Google Analytics (http://google.com/analytics)
+ */
+angular.module('angulartics.google.analytics.cordova', ['angulartics'])
+.config(['$analyticsProvider', function ($analyticsProvider, googleAnalyticsCordovaProvider) {
+  googleAnalyticsCordovaProvider.ready(function (analytics, success, failure) {
+    $analyticsProvider.registerPageTrack(function (path) {
+      analytics.trackPage(success, failure, path);
+    });
+
+    $analyticsProvider.registerEventTrack(function (action, properties) {
+      analytics.trackEvent(success, failure, action, properties.label, properties.value, properties.scope);
+    });
+  });
+}])
+
+.provider('googleAnalyticsCordova', function () {
+  function GoogleAnalyticsCordova($q, $log, ready, debug, trackingId, period) {
+    var deferred = $q.defer();
+    var deviceReady = false;
+
+    window.addEventListener('deviceReady', function () {
+      deviceReady = true;
+      deferred.resolve();
+    });
+
+    setTimeout(function () {
+      if (!deviceReady) {
+        deferred.resolve();
+      }
+    }, 3000);
+
+    function success() {
+      if (debug) {
+        $log.info(arguments);
+      }
+    }
+
+    function failure(err) {
+      if (debug) {
+        $log.error(err);
+      }
+    }
+
+    this.init = function () {
+      return deferred.promise.then(function () {
+        var analytics = window.plugins && window.plugins.gaPlugin;
+        if (analytics) {
+          analytics.init(analytics, function () {
+            ready(analytics, success, failure);
+          }, failure, trackingId, period || 10);
+        } else if (debug) {
+          $log.error('Google Analytics for Cordova is not available');
+        }
+      });
+    };
+  }
+
+  return {
+    $get: function () {
+      return angular.instantiate(GoogleAnalyticsCordova, {
+        ready: this._ready || angular.noop,
+        debug: this.debug,
+        trackingId: this.trackingId,
+        period: this.period
+      });
+    },
+    ready: function (fn) {
+      this._ready = fn;
+    }
+  };
+})
+
+.run(function (googleAnalyticsCordova) {
+  googleAnalyticsCordova.init();
+});
+
+})(angular);
