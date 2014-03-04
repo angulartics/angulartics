809827a5fa2267fee34427e8e77cbb4dc59fa6ef
Luis Farzati <lfarzati@gmail.com>
2014-03-04 15:26:24 -0300

    0.12.12 packaged

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,7 +43,9 @@ module.exports = function(grunt) {
                'dist/angulartics-ga-cordova.min.js': ['src/angulartics-ga-cordova.js'],
                'dist/angulartics-kissmetrics.min.js': ['src/angulartics-kissmetrics.js'],
                'dist/angulartics-mixpanel.min.js': ['src/angulartics-mixpanel.js'],
-               'dist/angulartics-segmentio.min.js': ['src/angulartics-segmentio.js']
+               'dist/angulartics-segmentio.min.js': ['src/angulartics-segmentio.js'],
+               'dist/angulartics-gtm.min.js': ['src/angulartics-gtm.js'],
+               'dist/angulartics-woopra.min.js': ['src/angulartics-woopra.js']
             }
          }
       },
