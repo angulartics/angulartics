f6a49d5dd6ff5febb483f9fcd4513b30729ecd2c
Chris Mendis <chris.mendis@gmail.com>
2014-05-21 21:13:52 -0400

    Fix luisfarzati/angulartics#110: ngRoute and ui-router support
    Trying to be routing-agnostic isn't working for us
    (see luisfarzati/angulartics#86 and luisfarzati/angulartics#114).
    
    This PR intends to re-introduce specific support for the two most popular
    routing modules: ngRoute and ui-router.
    
    Tests have been updated to cover ngRoute and ui-router. I've implicitly
    documented the minimum supported versions of both modules, using bower.json.
    
    TODO:
    
    * Update the README to explicitly document minimum supported versions of the
      routing modules.

--- karma.conf.js
+++ karma.conf.js
@@ -9,6 +9,8 @@ module.exports = function(config) {
 
     files: [
       'components/angular/angular.js',
+      'components/angular-route/angular-route.js',
+      'components/angular-ui-router/release/angular-ui-router.js',
       'components/angular-mocks/angular-mocks.js',
       'src/**/*.js',
       'test/**/*.js'
