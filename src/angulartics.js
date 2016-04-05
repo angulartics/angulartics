7f7843ffd639012daab5d2a4653bcb3acccf28e6
Oded Niv <oded.niv@gmail.com>
2016-04-05 16:37:30 +0300

    configurable exception tracking

--- src/angulartics.js
+++ src/angulartics.js
@@ -42,6 +42,7 @@ function $analytics() {
     },
     eventTracking: {},
     bufferFlushDelay: 1000, // Support only one configuration for buffer flush delay to simplify buffering
+    trackExceptions: false,
     developerMode: false // Prevent sending data in local/development environment
   };
 
@@ -128,6 +129,7 @@ function $analytics() {
       this.settings.pageTracking.basePath = (value) ? angular.element(document).find('base').attr('href') : '';
     },
     withAutoBase: function (value) { this.settings.pageTracking.autoBasePath = value; },
+    trackExceptions: function (value) { this.settings.trackExceptions = value; },
     developerMode: function(value) { this.settings.developerMode = value; }
   };
 
@@ -318,8 +320,12 @@ function analyticsOn($analytics) {
 function exceptionTrack($provide) {
   $provide.decorator('$exceptionHandler', ['$delegate', '$injector', function ($delegate, $injector) {
     function (error, cause) {
-      $injector.get('$analytics').exceptionTrack(error, cause);
-      return $delegate(error, cause);
+      var result = $delegate(error, cause);
+      var $analytics = $injector.get('$analytics');
+      if ($analytics.settings.trackExceptions) {
+        $analytics.exceptionTrack(error, cause);
+      }
+      return result;
     }
   }]);
 }
